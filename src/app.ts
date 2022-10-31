8e6071c1f235ba0544f1ab92b499032f509c0d8f
jihwooon <jihwooon@gmail.com>
2022-11-01 01:07:07 +0900

    app를 설정하라

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import config from 'config';
 import log from "./logger"
+import routes from "./routes";
+import connect from './db/connect';
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
@@ -13,4 +15,8 @@ app.use(express.urlencoded({ extended: false}))
 
 app.listen(port, host, () => {
     log.info(`Server listing at http://${host}:${port}`);
+
+    connect()
+    
+    routes(app)
 })
