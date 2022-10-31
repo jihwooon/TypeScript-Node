96c375231f4f6af6537225cb509eb3a1935a69d7
jihwooon <jihwooon@gmail.com>
2022-11-01 01:06:24 +0900

    label test1
    label test2
    
    label test3

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,27 @@
+import {Request, Response} from 'express'
+import { omit } from 'lodash'
+import {createUser} from '../service/user.service'
+import log from "../logger";
+
+export async function createUserHandler(req: Request, res: Response) {
+    try {
+        const user = await createUser(req.body);
+        return res.send(omit(user.toJSON(), "password"))
+    }catch(e: any) {
+        log.error(e);
+        return res.status(409).send(e.message);
+    }
+}
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+    // validate the email and password
+
+    // Create a session
+
+    // Create access token
+
+    // create refresh token
+
+    // send refresh & access token back
+
+}
