c0d3185cfd3346f521176c56211f80c4b12a8393
jihwooon <jihwooon@gmail.com>
2022-11-06 10:42:24 +0900

    post domin을 작성하라

--- src/routes.ts
+++ src/routes.ts
@@ -1,67 +1,53 @@
 import { Express, Request, Response } from "express";
 import { createUserHandler } from './controller/user.controller'
-import validateRequest from './middleware/validateRequest'
-import {createUserSchema, createUserSessionSchema } from './schema/user.schema'
-import { createUserSessionHandler } from './controller/session.controller'
+import { validateRequest, requiresUser } from './middleware'
+import { createUserSchema, createUserSessionSchema } from './schema/user.schema'
+import { createUserSessionHandler, invalidateUserSessionHandler, getUserSessionsHandler } from './controller/session.controller'
+import { createPostSchema, updatePostSchema, deletePostSchema } from './schema/post.schema'
+import { createPostHandler, deletePostHandler, getPostHandler, updatePostHandler } from './controller/post.controller'
 
-export default function(app:Express) {
+export default function (app: Express) {
 
-    /**
-    * @openapi
-    * /healthcheck:
-    *  get:
-    *     tags:
-    *     - Healthcheck
-    *     description: Responds if the app is up and running
-    *     responses:
-    *       200:
-    *         description: App is up and running
-    */
-    app.get('/healthcheck', (req: Request, res:Response) => res.sendStatus(200))
+    app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
 
-
-    /**
-     * @openapi
-     * '/api/users':
-     * post:
-     *  tags:
-     *  - User
-     *      summary: Register a user
-     *      requestBody:
-     *        required: true
-     *        content:
-     *          application:/json:
-     *              schema:
-     *                 $ref: '#/components/schemas/CreateUserInput'
-     *      responses:
-     *      200:
-     *        description: Success
-     *        content:
-     *          application/json:
-     *              schema:
-     *                 $ref: '#/components/schemas/CreateUserResponse'
-     *      409:
-     *        description: Conflict
-     *      400:
-     *        description: Bad request
-     */
-    app.post('/api/users', validateRequest(createUserSchema), createUserHandler);
+    app.post(
+        '/api/users',
+        validateRequest(createUserSchema),
+        createUserHandler
+    );
 
     // POST /api/sessions
     app.post('/api/sessions', validateRequest(createUserSessionSchema), createUserSessionHandler);
-    
-    // Login
 
-    
-    // POST /api/user
-    
+    // GET /api/sessions
+    app.get("/api/sessions", requiresUser, getUserSessionsHandler)
 
-    // GET the user's sessions
-// GET /api/sessions
+    // Logout
+    // DELETE /api/sessions
+    app.delete("/api/sessions", requiresUser, invalidateUserSessionHandler);
 
-// Logout
-// DELETE /api/sessions
+    // Create a post
+    app.post(
+        "/api/posts",
+        [requiresUser, validateRequest(createPostSchema)],
+        createPostHandler
+    );
 
-// GET /api/posts /api/posts/postId
+    // Update a post
+    app.put(
+        "/api/posts/:postId",
+        [requiresUser, validateRequest(updatePostSchema)],
+        updatePostHandler
+    );
+
+    // Get a post
+    app.get("/api/posts/:postId", getPostHandler);
+
+    // Delete a post
+    app.delete(
+        "/api/posts/:postId",
+        [requiresUser, validateRequest(deletePostSchema)],
+        deletePostHandler
+    );
 
 }
