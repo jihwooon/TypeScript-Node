cb32413cf1286571fc90dbe412b67874aceaaa98
jihwooon <jihwooon@gmail.com>
2022-11-12 18:01:44 +0900

    openapi를 작성하라

--- src/routes.ts
+++ src/routes.ts
@@ -5,10 +5,46 @@ import {createUserSchema, createUserSessionSchema } from './schema/user.schema'
 import { createUserSessionHandler } from './controller/session.controller'
 
 export default function(app:Express) {
- 
+
+    /**
+    * @openapi
+    * /healthcheck:
+    *  get:
+    *     tags:
+    *     - Healthcheck
+    *     description: Responds if the app is up and running
+    *     responses:
+    *       200:
+    *         description: App is up and running
+    */
     app.get('/healthcheck', (req: Request, res:Response) => res.sendStatus(200))
 
-    // Register user
+
+    /**
+     * @openapi
+     * '/api/users':
+     * post:
+     *  tags:
+     *  - User
+     *      summary: Register a user
+     *      requestBody:
+     *        required: true
+     *        content:
+     *          application:/json:
+     *              schema:
+     *                 $ref: '#/components/schemas/CreateUserInput'
+     *      responses:
+     *      200:
+     *        description: Success
+     *        content:
+     *          application/json:
+     *              schema:
+     *                 $ref: '#/components/schemas/CreateUserResponse'
+     *      409:
+     *        description: Conflict
+     *      400:
+     *        description: Bad request
+     */
     app.post('/api/users', validateRequest(createUserSchema), createUserHandler);
 
     // POST /api/sessions
